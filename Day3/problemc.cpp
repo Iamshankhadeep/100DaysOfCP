1d745c609bb29c04af39cb1a39ff56b30174ba46
Shankhadeep <shankhadeepdey99@gmail.com>
2019-11-08 19:56:48 +0530

    made sure problemc.cpp produce accepted result

--- Day3/problemc.cpp
+++ Day3/problemc.cpp
@@ -7,36 +7,48 @@ int main(){
     vector<int> lk;
     int mod = 1e9+7;
     cin>>k;
+    vector<int> dp(k.size()+1);
+    dp[0]=1;dp[1]=1;
     if(k.find('m')<k.size() || k.find('w') < k.size()){
         cout<<"0"<<endl;
     }
     else{
-        for (int i = 0; i < k.size(); i++)
-        {
-            long long int count = 0;
-            if(k[i]=='n'){
-                while(k[i]=='n'){
-                    count++;
-                    i++;
-                }
-                lk.push_back(count);
-                i--;
-            }
-            else if(k[i]=='u'){
-                while(k[i]=='u'){
-                    count++;
-                    i++;
-                }
-                lk.push_back(count);
-                i--;
+        for(int i=2;i<=k.size();i++){
+            dp[i]=dp[i-1];
+            if(k[i-1]==k[i-2] && (k[i-1]=='u' || k[i-1]=='n')){
+                dp[i]=dp[i]+dp[i-2];
+                dp[i] %= mod;
             }
         }
-        long long int val = 1;
-        for (int i = 0; i < lk.size(); i++)
-        {
-            val *= lk[i];
-            val %= mod;
-        }
-        cout<<val<<endl;
+        cout<<dp[k.size()]<<endl;
     }
+    // else{
+    //     for (int i = 0; i < k.size(); i++)
+    //     {
+    //         long long int count = 0;
+    //         if(k[i]=='n'){
+    //             while(k[i]=='n'){
+    //                 count++;
+    //                 i++;
+    //             }
+    //             lk.push_back(count);
+    //             i--;
+    //         }
+    //         else if(k[i]=='u'){
+    //             while(k[i]=='u'){
+    //                 count++;
+    //                 i++;
+    //             }
+    //             lk.push_back(count);
+    //             i--;
+    //         }
+    //     }
+    //     long long int val = 1;
+    //     for (int i = 0; i < lk.size(); i++)
+    //     {
+    //         val *= lk[i];
+    //         val %= mod;
+    //     }
+    //     cout<<val<<endl;
+    // }
 }
