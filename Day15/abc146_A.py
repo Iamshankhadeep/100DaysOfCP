1c1dc728f410bbeb0dec7587e1455847761ebecf
Shankhadeep <shankhadeepdey99@gmail.com>
2019-11-25 00:01:19 +0530

    upsolve atcoder 146

---
+++ Day15/abc146_A.py
@@ -0,0 +1,3 @@
+v = {'SUN' : 0, 'MON' : 1, 'TUE' : 2, 'WED' : 3, 'THU' : 4, 'FRI' : 5, 'SAT' : 6}
+a = input()
+print(7-v[a])
---
+++ Day15/abc146_c.cpp
@@ -0,0 +1,56 @@
+#include "bits/stdc++.h"
+using namespace std;
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+
+using ll = long long;
+
+const int INF = 1e9 + 5;
+#define FOR(i, a, b) for (int i=a; i<(b); i++)
+#define F0R(i, a) for (int i=0; i<(a); i++)
+#define FORd(i,a,b) for (int i = (b)-1; i >= a; i--)
+#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
+
+int main() {
+    ll A, B, X; cin >> A >> B >> X;
+    if (A * 1000000000 + B * 10 <= X) {
+        cout << 1000000000 << endl; return 0;
+    }
+ 
+    FORd(dig, 0, 9) {
+        ll powTen = 1;
+        F0R(i, dig) powTen *= 10;
+        if (powTen * A + B * (dig+1) <= X) {
+            X -= B * (dig+1);
+            cout << min(X / A, powTen*10-1) << endl;
+            return 0;
+        }
+    }
+    cout << 0 << endl;
+}
